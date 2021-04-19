abc05696febf58463e7e348b0bc0c3f322b6ab51
benfanderson <benfanderson81@gmail.com>
2021-04-19 09:45:06 +0800

    lead-story and other story styles now in sass

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { Switch, Route, HashRouter } from 'react-router-dom';
 import storiesArray from './stories.json';
 import FrontPage from './FrontPage';
 import Story from './Story';
-// import NotFound from './NotFound';
 
 function App() {
   const [stories] = useState(storiesArray);
 
   const myStories = Object.keys(stories).map((key) => stories[key]);
-
-  //   return (<Stories stories={myStories} />);
 
   return (
     <>
